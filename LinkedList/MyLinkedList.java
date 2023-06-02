4a96726df2d284470ddd362a2d71a19a0ea997c5
Olga Kutueva <kutueva.olga@gmail.com>
2023-06-02 18:33:14 +0300

    MyLinkedList class

---
+++ LinkedList/MyLinkedList.java
@@ -0,0 +1,22 @@
+package LinkedList;
+
+public class MyLinkedList {
+
+    private Node head;
+
+    public class Node {
+        int value;
+        Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        public Node(int value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    
+}
