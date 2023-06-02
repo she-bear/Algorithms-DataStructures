0f98a42609bf61e567952a156e590582b95dd395
Olga Kutueva <kutueva.olga@gmail.com>
2023-06-02 17:27:22 +0300

    Linked List initial

---
+++ LinkedList/Main.java
@@ -0,0 +1,15 @@
+package LinkedList;
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        int[] array = new int[100];
+
+        int a = array[76]; // перебора массива НЕ БУДЕТ, сразу обращение к нужному элементу
+        
+         // Linked List
+         // head <-> ... <-> ... <-> tail  
+    }
+}
