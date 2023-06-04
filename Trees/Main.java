052f67af10c605dad0539090a5242509ee8cb8e6
Olga Kutueva <kutueva.olga@gmail.com>
2023-06-04 19:38:11 +0300

    Directory renamed

--- Trees/Main.java
+++ Trees/Main.java
@@ -1,4 +1,4 @@
-package HashTablesAndTrees;
+package Trees;
 
 public class Main {
     public static void main(String[] args) {
--- Trees/Tree.java
+++ Trees/Tree.java
@@ -1,4 +1,4 @@
-package HashTablesAndTrees;
+package Trees;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
